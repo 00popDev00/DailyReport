4c206bc43323f00c4d20cee3544c5db57c3963a8
00popDev00 <puravofficial@gmail.com>
2021-07-28 22:15:33 +0530

    setup webpack with simple react

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+// import App from "./App";
+// import "./App.css";
+
+ReactDom.render(<div>Hi!</div>, document.getElementById("app"));
