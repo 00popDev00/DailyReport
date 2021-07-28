116fef404693e864637c8c9073fe60e99684cad0
00popDev00 <puravofficial@gmail.com>
2021-07-28 23:01:19 +0530

    webpack config for react-router-dom

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // Where files should be sent once they are bundled
+  mode: "development",
+  entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index.bundle.js",
   },
-  // webpack 5 comes with devServer which loads in development mode
   devServer: {
     port: 3000,
     watchContentBase: true,
+    historyApiFallback: true,
   },
-  // Rules of how webpack will take our files, complie & bundle them for the browser
   module: {
     rules: [
       {
